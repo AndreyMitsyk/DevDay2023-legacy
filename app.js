f3bc5bef82d16df132fbafd0ae35fa8a819679a1
Andrey Mitsyk <andrey.mitsyk@roadmunk.com>
2023-03-18 16:23:28 +0100

    Changed project to module

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-const express = require('express');
+import express from 'express';
+import { getHello } from './src/my-legacy-service.js';
+
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hi from Express!');
+  res.send(getHello());
 });
 
 app.listen(port, () => {
---
+++ src/my-legacy-service.js
@@ -0,0 +1,3 @@
+export function getHello() {
+  return 'Hi from Express!';
+}
